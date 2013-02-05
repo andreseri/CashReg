ef2a5aa5ec04ec0080a7679428af1cbe411256df
André Seri <seri.andre@gmail.com>
2013-02-05 17:55:31 +0000

    CashReg 1.0

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready( function() {
+	var total = 0;
+	$('#addItem').click(function() {
+		var toAddItem = $('input[name= item]').val();
+		var toAddPrice = parseInt($('input[name= price]').val());
+
+		console.log(typeof toAddPrice);
+		$('.registerList').append('<tr class="listItem"><td>' + toAddItem + '</td><td>' + toAddPrice + '</td></tr>');
+		total += toAddPrice;
+		$('.total').html("$ " + total);
+	});
+
+	$(document).on('click', '.listItem', function() {
+		$(this).remove();
+	});
+});
